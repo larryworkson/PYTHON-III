b61b54c0a0393a7d10d0ccb78f9c31d770eb6f0b
Larry Rocha <143459283+larryworkson@users.noreply.github.com>
2023-12-18 21:07:46 -0300

    melhora no modelo
    o modelo foi melhorado, porém está muito rígido na análise, avaliando clientes medianos como ruins e clientes bons como medianos

--- DATA-SCIENCE/dataenv/ex014-analise-credito.py
+++ DATA-SCIENCE/dataenv/ex014-analise-credito.py
@@ -4,7 +4,7 @@ import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, classification_report
-from funcoes.pontuacao_credito import calcular_pontuacao
+from funcoes.pontuacao_credito import *
 
 
 df = pd.read_csv('files/clientes-2.csv')
@@ -14,10 +14,16 @@ df_nums = df[num_cols] #craindo DF somente com números
 print(df_nums.corr())
 
 #calculando a pontução
-for c in range(0, len(df)):
+""" for c in range(0, len(df)):
     df['pontuação'] = df.apply(lambda x: calcular_pontuacao(x), axis=1)
 
-#df.to_csv('files/clientes-2.csv', index=False)
+df.to_csv('files/clientes-2.csv', index=False) """
+
+""" #definindo historico
+for c in range(0, len(df)):
+    df['historico'] = df.apply(lambda z: definir_historico(z), axis=1)
+
+df.to_csv('files/clientes-2.csv', index=False) """
 
 x = df.drop(['cliente', 'historico'], axis=1)
 y = df['historico']
@@ -31,6 +37,26 @@ cm = confusion_matrix(y_previsto, y_teste)
 print(classification_report(y_previsto, y_teste))
 
 
+while True:
+    print('-'*30)
+    print('Nova previsão')
+    print('-'*30)
+    idade = int(input('Idade: '))
+    renda = float(input('Renda: '))
+    divida = float(input('Dívida: '))
+    cliente = {'idade': idade,
+                    'renda': renda,
+                    'divida': divida}
+    pts = calcular_pontuacao(cliente)
+    cliente['pontuação'] = pts
+    novos_dados = pd.DataFrame([cliente])
+    previsao = modelo.predict(novos_dados)
+    print(f'Este cliente tem o histórico: {previsao}')
+    resp = str(input('Digite N para sair: ')).upper()
+    if resp == 'N':
+        break
+
+
 # python ex014-analise-credito.py
 
-"""a correlação está dizendo que a dívida não gera impacto sobre a pontuação e a idade impacta muito pouco. Precisa ajustar os dados para aprimorar o modelo."""
+"""o modelo acertando, porém está sendo bastante conservador na análise. Está perdendo clientes com potencial de compra"""
--- DATA-SCIENCE/dataenv/funcoes/pontuacao_credito.py
+++ DATA-SCIENCE/dataenv/funcoes/pontuacao_credito.py
@@ -5,7 +5,7 @@ def calcular_pontuacao(linha):
 
     # Defina pesos para cada variável
     peso_renda = 25
-    peso_divida = -15
+    peso_divida = 20
     
     # Ajuste da pontuação com base na idade
     if int(idade) <= 30:
@@ -17,7 +17,7 @@ def calcular_pontuacao(linha):
     pontuacao += (renda / 100) * peso_renda
     
     # Ajuste da pontuação com base na dívida
-    pontuacao += (divida / 100) * peso_divida
+    pontuacao -= (divida / 100) * peso_divida
     
     # Garante que a pontuação final esteja dentro de um intervalo específico
     if pontuacao > 850:
@@ -27,6 +27,17 @@ def calcular_pontuacao(linha):
     
     return pontuacao
 
+
+def definir_historico(linha):
+    pts = linha['pontuação']
+    if pts >= 700:
+        linha['historico'] = 'Bom'
+    elif pts >= 500 and pts < 700:
+        linha['historico'] = 'Regular'
+    elif pts < 500:
+        linha['historico'] = 'Ruim'
+
+    return linha['historico']
 
 
 
@@ -58,12 +69,17 @@ def calcular_pontuacao_teste(idade, renda, divida):
     return pontuacao
 
 
-""" idade_cliente = int(input('Idade: '))
-renda_cliente = float(input('Renda: '))
-divida_cliente = float(input('Dívida: '))
 
-pontuacao_final = calcular_pontuacao_teste(idade_cliente, renda_cliente, divida_cliente)
-print(f'A pontuação de crédito é: {pontuacao_final}') """
+""" while True:
+    idade_cliente = int(input('Idade: '))
+    renda_cliente = float(input('Renda: '))
+    divida_cliente = float(input('Dívida: '))
+
+    pontuacao_final = calcular_pontuacao_teste(idade_cliente, renda_cliente, divida_cliente)
+    print(f'A pontuação de crédito é: {pontuacao_final}')
+    resp = str(input('N para sair: ')).lower()
+    if resp == 'n':
+        break """
 
 
 # python pontuacao_credito.py
