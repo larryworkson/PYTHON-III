90c420a99150ffb4e9e1416f6d9b7b56f1481dd3
Larry Rocha <143459283+larryworkson@users.noreply.github.com>
2024-07-13 17:22:31 -0300

    s

--- GERAL/RPA/00-ex-01/robot01.py
+++ GERAL/RPA/00-ex-01/robot01.py
@@ -1,4 +1,3 @@
-import rpa as r
 import pyautogui as p
 
 #robo abre camera do computador e captura tela salvando uma imagem no desktop.
@@ -7,10 +6,11 @@ p.typewrite('cmd')
 p.press('enter')
 p.sleep(1)
 p.typewrite('start microsoft.windows.camera:')
+p.press('enter')
 p.sleep(3)
 ps = p.screenshot()
 ps.save('C:/Users/studi/OneDrive/Área de Trabalho/telacapturada.png')
-valor = p.getActiveWindow()
-valor.close()
+jn1 = p.getActiveWindow()
+jn1.close()
 
 #   python robot01.py
--- GERAL/RPA/00-ex-01/robot07.py
+++ GERAL/RPA/00-ex-01/robot07.py
@@ -1,9 +1,31 @@
-import rpa as r
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 import pyautogui as p
-
-r.init()
-r.url('https://google.com')
+import pandas as pd
 
 
-#'C:\Users\studi\AppData\Roaming'
+def baixar_plan():
+    #baixando planilha
+    driver = webdriver.Chrome()
+    driver.get('https://rpachallenge.com/assets/downloadFiles/challenge.xlsx')
+    p.sleep(3)
+    return 'Dados baixados com sucesso!'
+
+
+def busca_last_name():
+    #abrindo o site e identificando o campo ÚLTIMO NOME
+    driver = webdriver.Chrome()
+    driver.get('https://rpachallenge.com/')
+    elemento = driver.find_element(By.XPATH, '//label[text()="Last Name"]')
+    txt = elemento.text
+    driver.quit()
+    return txt
+
+def banco_dados():
+    dados = pd.read_excel('"C:\Users\studi\Downloads\challenge.xlsx"', sheet_name='Sheet1')
+    df = pd.DataFrame(dados, columns=['First Name', 'Last Name ', 'Company Name', 'Role in Company', 'Address', 'Email', 'Phone Number'])
+
+
+
+#'https://rpachallenge.com/'
 #   python robot07.py
