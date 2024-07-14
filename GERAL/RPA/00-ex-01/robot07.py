a8e629f8b19b978827327e36cd7a90a1d206abe4
Larry Rocha <143459283+larryworkson@users.noreply.github.com>
2024-07-13 21:37:39 -0300

    bots

--- GERAL/RPA/00-ex-01/robot07.py
+++ GERAL/RPA/00-ex-01/robot07.py
@@ -12,20 +12,47 @@ def baixar_plan():
     return 'Dados baixados com sucesso!'
 
 
-def busca_last_name():
+def busca_campo(x):
     #abrindo o site e identificando o campo ÚLTIMO NOME
     driver = webdriver.Chrome()
     driver.get('https://rpachallenge.com/')
-    elemento = driver.find_element(By.XPATH, '//label[text()="Last Name"]')
-    txt = elemento.text
+    elemento = driver.find_element(By.XPATH, f'//label[text()="{x}"]')
+    txt_campo = elemento.text
     driver.quit()
-    return txt
+    return txt_campo
 
-def banco_dados():
-    dados = pd.read_excel('"C:\Users\studi\Downloads\challenge.xlsx"', sheet_name='Sheet1')
+def insere_no_campo(lb, dado):
+        driver = webdriver.Chrome()
+        driver.get('https://rpachallenge.com/')
+        label = lb
+        atributo_for = label.get_attribute('for')
+        inserir = driver.find_element(By.ID, atributo_for)
+        inserir.send_keys(dado)
+        driver.quit()
+
+def clicar(class_name):
+    open = webdriver.Chrome()
+    open.get('https://rpachallenge.com/')
+    link = open.find_element(By.CLASS_NAME, class_name)
+    link.click()
+
+
+def preencher_form():
+    dados = pd.read_excel('C:/Users/studi/Documents/code2024/PYTHON-III/GERAL/RPA/00-ex-01/challenge.xlsx', sheet_name='Sheet1')
     df = pd.DataFrame(dados, columns=['First Name', 'Last Name ', 'Company Name', 'Role in Company', 'Address', 'Email', 'Phone Number'])
+    for row in df.itertuples(): # enviando os dados de cada coluna (df) por linha (row)
+        label = busca_campo('First Name')
+        insere_no_campo(label, row[1]) #ver forma de simplificar o loop
+        clicar('uiColorButton')
+
+        
 
 
+clicar('btn-large')
+p.sleep(2)
+preencher_form()
 
 #'https://rpachallenge.com/'
 #   python robot07.py
+
+'''fazer nova versão sem funções, o webdriver precisa realizar uma única sessão'''
