e8ec729c8ef2a1eefb5ec7ac029cb9dc7e2bf884
Larry Rocha <143459283+larryworkson@users.noreply.github.com>
2024-07-17 17:54:55 -0300

    up

--- GERAL/RPA/00-ex-01/robot09-dolar.py
+++ GERAL/RPA/00-ex-01/robot09-dolar.py
@@ -13,17 +13,17 @@ print(valor, type(valor))
 import requests
 from bs4 import BeautifulSoup
 
- response = requests.get(url)
+response = requests.get(url)
 
-    # Verificar se a requisição foi bem-sucedida
-    if response.status_code == 200:
-        # Usar BeautifulSoup para analisar o conteúdo HTML
-        soup = BeautifulSoup(response.content, 'html.parser')
+# Verificar se a requisição foi bem-sucedida
+if response.status_code == 200:
+    # Usar BeautifulSoup para analisar o conteúdo HTML
+    soup = BeautifulSoup(response.content, 'html.parser')
 
-        # Encontrar o elemento com a classe específica
-        elemento = soup.find('p', class_='PlayerSummary_playerStatValue___EDg_')
-        if elemento:
-            ppg = elemento.text
+    # Encontrar o elemento com a classe específica
+    elemento = soup.find('p', class_='PlayerSummary_playerStatValue___EDg_')
+    if elemento:
+        ppg = elemento.text
 
 
 #       python robot09-dolar.py
