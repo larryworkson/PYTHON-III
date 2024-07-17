26154d9f8648ed5274d09d8a4df0372e56bd3ba1
Larry Rocha <143459283+larryworkson@users.noreply.github.com>
2024-07-17 17:50:15 -0300

    up

--- GERAL/RPA/00-ex-01/robot08.py
+++ GERAL/RPA/00-ex-01/robot08.py
@@ -3,12 +3,17 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 import pyperclip
 
-driver = webdriver.Chrome()
-driver.get('https://www.google.com/maps/place/Sal%C3%A3o+Fio+a+Fio/@-27.6724565,-48.6842127,15z/data=!4m11!1m3!2m2!1ssalao+beleza!6e1!3m6!1s0x95273468663e28c3:0xc0a30e0816dd1608!8m2!3d-27.6755925!4d-48.6684515!15sCgxzYWxhbyBiZWxlemGSAQxiZWF1dHlfc2Fsb27gAQA!16s%2Fg%2F11d_84kt6l?entry=ttu')
-wait = WebDriverWait(driver, 10)
-#clicar no link
-btn = driver.find_element(By.CSS_SELECTOR, '[data-tooltip="Copiar número de telefone"]')
+""" driver = webdriver.Chrome()
+driver.get('https://www.google.com/maps/search/salao+beleza/@-27.6773724,-48.6817139,15z/data=!4m2!2m1!6e1?entry=ttu')
+wait = WebDriverWait(driver, 10) """
+clicar = webdriver.Chrome()
+clicar.get('https://www.google.com/maps/search/salao+beleza/@-27.6773724,-48.6817139,15z/data=!4m2!2m1!6e1?entry=ttu')
+links = clicar.find_elements(By.CLASS_NAME, 'hfpxzc')
+primeiro_link = links[0]
+primeiro_link.click()
+wait = WebDriverWait(clicar, 10)
+""" btn = clicar.find_elements(By.CLASS_NAME, "AeaXub")
 btn.click()
-print(pyperclip.paste())
+print(pyperclip.paste()) """
 
 #       python robot08.py
---
+++ GERAL/RPA/00-ex-01/robot09-dolar.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+driver = webdriver.Chrome()
+driver.get('https://www.melhorcambio.com/dolar-hoje')
+cotacao = driver.find_element(By.XPATH, "//input[@class='text-verde']")
+valor = cotacao.get_attribute('value')
+print(valor, type(valor))
+
+
+#capturando sem bot
+import requests
+from bs4 import BeautifulSoup
+
+ response = requests.get(url)
+
+    # Verificar se a requisição foi bem-sucedida
+    if response.status_code == 200:
+        # Usar BeautifulSoup para analisar o conteúdo HTML
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        # Encontrar o elemento com a classe específica
+        elemento = soup.find('p', class_='PlayerSummary_playerStatValue___EDg_')
+        if elemento:
+            ppg = elemento.text
+
+
+#       python robot09-dolar.py
