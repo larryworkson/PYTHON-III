9690558c58a9407d88e7ae3993182b4574d4e32a
Larry Rocha <143459283+larryworkson@users.noreply.github.com>
2024-07-18 17:56:18 -0300

    up

--- GERAL/03-Exercícios/Cotacao-dolar/robot09-dolar.py
+++ GERAL/03-Exercícios/Cotacao-dolar/robot09-dolar.py
@@ -1,29 +1,34 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
-
-driver = webdriver.Chrome()
-driver.get('https://www.melhorcambio.com/dolar-hoje')
-cotacao = driver.find_element(By.XPATH, "//input[@class='text-verde']")
-valor = cotacao.get_attribute('value')
-print(valor, type(valor))
-
-
-#capturando sem bot
 import requests
 from bs4 import BeautifulSoup
 
-response = requests.get(url)
+def cotacao_dolar():
+    """Busca cotação do dolar"""
+    driver = webdriver.Chrome()
+    driver.get('https://www.melhorcambio.com/dolar-hoje')
+    cotacao = driver.find_element(By.XPATH, "//input[@class='text-verde']")
+    valor = cotacao.get_attribute('value')
+    return valor
 
-# Verificar se a requisição foi bem-sucedida
-if response.status_code == 200:
-    # Usar BeautifulSoup para analisar o conteúdo HTML
-    soup = BeautifulSoup(response.content, 'html.parser')
 
-    # Encontrar o elemento com a classe específica
-    elemento = soup.find('p', class_='PlayerSummary_playerStatValue___EDg_')
-    if elemento:
-        ppg = elemento.text
+def cotacao_dolar_request():
+    # Verificar se a requisição foi bem-sucedida
+    try:
+        response = requests.get('https://www.remessaonline.com.br/cotacao/cotacao-dolar')
+        if response.status_code == 200:
+            # Usar BeautifulSoup para analisar o conteúdo HTML
+            soup = BeautifulSoup(response.content, 'html.parser')
+            # Encontrar o elemento com a classe específica
+            elemento = soup.find('div', class_='ljisZu').text
+    except NameError as e:
+        print(e)
+    cotacao = elemento[:4].replace(' ','')
+    valor = cotacao.replace(',', '.')
+    dolar = float(valor)
+    return print(dolar, type(dolar))
+
+cotacao_dolar_request()
 
 
 #       python robot09-dolar.py
